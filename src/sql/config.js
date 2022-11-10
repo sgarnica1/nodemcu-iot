0a055b971c19e712457cb6e94824470f5e562d53
Sergio Garnica <sgarnica1902@gmail.com>
2022-11-10 13:04:09 -0600

    Sequelize config

--- src/sql/config.js
+++ src/sql/config.js
@@ -15,7 +15,7 @@ sequelize
     console.log("Connection to MySQL has been established successfully.");
   })
   .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+    console.error("Unable to connect to the database:", err.message);
   });
 
 module.exports = sequelize;
