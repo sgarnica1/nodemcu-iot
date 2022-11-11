2e7dceddc55c908bea9fe9ac4fa008a82c81a1a1
Sergio Garnica <sgarnica1902@gmail.com>
2022-11-11 09:21:42 -0600

    Return query and headers on get endpoint

--- src/controllers/TempHumidity.controller.js
+++ src/controllers/TempHumidity.controller.js
@@ -1,17 +1,21 @@
 const TempHumidity = require("../models/TempHumidity");
 const getData = async (req, res) => {
-  console.log(req.query)
-  // res.json({query: req.body});
-  if (req.query.temperature && req.query.humidity) {
-    createData(req, res);
-    return;
-  }
-  try {
-    const data = await TempHumidity.find();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  res.json({
+    args: req.query,
+    headers: req.headers,
+  })
+  // console.log(req.query)
+  // // res.json({query: req.body});
+  // if (req.query.temperature && req.query.humidity) {
+  //   createData(req, res);
+  //   return;
+  // }
+  // try {
+  //   const data = await TempHumidity.find();
+  //   res.status(200).json(data);
+  // } catch (err) {
+  //   res.status(500).json({ message: err.message });
+  // }
 };
 const createData = async (req, res) => {
   const data = new TempHumidity({
